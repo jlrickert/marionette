1ce3e0e9b0a22d4de22e89efdc3fc6602fc9fc6a
Jared Rickert <jaredrickert52@gmail.com>
2017-03-19 16:03:41 -0500

    refactored a few things and fixed bootstraping vagrant

--- bin/bootstrap-vagrant.sh
+++ bin/bootstrap-vagrant.sh
@@ -5,33 +5,43 @@ ROOT_DIR=$(dirname $BIN_DIR)
 COUNTRY='US'
 PRIVATE_KEY=$ROOT_DIR/.vagrant/machines/default/virtualbox/private_key
 INVENTORY=$ROOT_DIR/dev-inventory.ini
+VAGRANT_IP=`grep -Eo "^([0-9]{3}\.){3}[0-9]{3}" $INVENTORY`
 
 cd $ROOT_DIR
 
 export ANSIBLE_HOST_KEY_CHECKING='False'
 
 vagrant up
-sleep 2 # make sure ssh is up and running
+
+echo "Waiting for ssh server to be up"
+if ! ssh -q vagrant@$VAGRANT_IP -i $PRIVATE_KEY exit; then
+    echo "ssh timeout"
+    exit 1;
+fi
 
 MIRROR_URL="https://www.archlinux.org/mirrorlist/?country=${COUNTRY}&protocol=http&ip_version=4&use_mirror_status=on"
-if /usr/bin/curl --silent --fail --output mirrorlist "${MIRROR_URL}"; then
+MIRRORLIST=/tmp/${USER}/mirrorlist
+mkdir -p $(dirname $MIRRORLIST)
+if /usr/bin/curl --silent --fail --output "${MIRRORLIST}" "${MIRROR_URL}"; then
     case $OSTYPE in
         darwin*)
-            /usr/bin/sed -i '' 's/#Server/Server/g' mirrorlist
+            /usr/bin/sed -i '' 's/#Server/Server/g' $MIRRORLIST
             ;;
         linux*)
-            /usr/bin/sed -i 's/#Server/Server/g' mirrorlist
+            /usr/bin/sed -i 's/#Server/Server/g' $MIRRORLIST
             ;;
     esac
+    echo "Updating pacman mirros"
     ansible vagrant \
             -m copy \
             -i $INVENTORY \
             --sudo \
             --private-key=$PRIVATE_KEY \
-            -a 'src=mirrorlist dest=/etc/pacman.d/mirrorlist owner=root group=root mode=0644 backup=yes'
-    rm mirrorlist
+            -a "src=${MIRRORLIST} dest=/etc/pacman.d/mirrorlist owner=root group=root mode=0644 backup=yes"
+    rm $MIRRORLIST
 fi
 
+echo "Ensure that ansible is present"
 ansible vagrant \
         -i $INVENTORY \
         -m raw \
