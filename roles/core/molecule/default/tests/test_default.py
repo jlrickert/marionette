051f70651b9c596cdc027a85259e1e0a636830f9
Jared Rickert <jaredrickert52@gmail.com>
2019-09-20 16:50:54 -0500

    added dotfile installation for core role

--- roles/core/molecule/default/tests/test_default.py
+++ roles/core/molecule/default/tests/test_default.py
@@ -17,3 +17,13 @@ def test_user(host):
     assert os.path.exists(os.path.join(user.home, ".ssh"))
     assert user.shell == "/usr/bin/zsh"
     assert "sudo" in user.groups
+
+
+def test_dotfiles(host):
+    assert host.package("chezmoi").is_installed
+    chezmoi_config = host.file("/home/jlrickert/.config/chezmoi/chezmoi.toml")
+    assert chezmoi_config.contains("signing_key")
+    assert chezmoi_config.contains('name = "Jared Rickert"')
+    assert chezmoi_config.contains('email = "jaredrickert52@gmail.com"')
+    assert host.file("/home/jlrickert/.gitconfig").contains(
+        'name = "Jared Rickert"')
