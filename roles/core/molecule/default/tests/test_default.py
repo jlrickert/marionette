30abfee8d5672b324362272325087590290d8ea6
Jared Rickert <jaredrickert52@gmail.com>
2019-09-20 12:32:43 -0500

    working minimal core role that pass tests

--- roles/core/molecule/default/tests/test_default.py
+++ roles/core/molecule/default/tests/test_default.py
@@ -1,5 +1,4 @@
 import os
-
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -7,9 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_packages(host):
+    for name in ["ssh", "python3", "python3-pip"]:
+        pkg = host.package(name)
+        assert pkg.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_user(host):
+    user = host.user("jlrickert")
+    assert os.path.exists(os.path.join(user.home, ".ssh"))
+    assert user.shell == "/usr/bin/zsh"
+    assert "sudo" in user.groups
