83a6e36b58834694333d8c4798e5b5167745252a
Jared Rickert <jaredrickert52@gmail.com>
2019-11-06 19:07:39 -0600

    added scala and java to language role

--- roles/language/molecule/default/tests/test_default.py
+++ roles/language/molecule/default/tests/test_default.py
@@ -8,6 +8,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_node_version(host):
-    node_version = host.run("node --version")
-    print(node_version)
-    assert node_version.stdout == "v13.0.1\n"
+    node_version: str = host.run("node --version").stdout
+    assert "v13" in node_version, node_version.stdout
+
+
+def test_scala_version(host):
+    scala_version = host.run("sbt --version").stdout
+    assert "sbt version" in scala_version, scala_version
+
+
+def test_java_version(host):
+    java_version = host.run("java --version").stdout
+    assert "openjdk" in java_version, java_version
